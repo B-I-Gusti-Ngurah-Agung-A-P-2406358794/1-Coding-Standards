cc81a2f80e2a46c88f3024ef631bd0d325f5d3d5
gung16 <gung.160906@gmail.com>
2026-02-09 21:20:50 +0700

    Made product test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdrivermanagerVersion = "5.6.3"
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.10"
@@ -7,10 +11,6 @@ plugins {
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 description = "eshop"
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
 
 java {
     toolchain {
@@ -29,18 +29,16 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation ("org. seleniumha selenium:selenium-java:$seleniumJavaVersion")
-    testImplementation("io github bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
-    testImplementation ("io github bonigarcia:webdrivermanager: $webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testRuntimeOnly("org junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
 }
 
 tasks.register<Test>("unitTest") {
@@ -55,6 +53,7 @@ tasks.register<Test>("unitTest") {
 tasks.register<Test>("functionalTest") {
     description = "Runs functional tests."
     group = "verification"
+
     filter {
         includeTestsMatching("*FunctionalTest")
     }
